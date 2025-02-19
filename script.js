69fe0c204e0c84a30508e259f08fa1c5ec2c83ad
AndreyJustino <andreyjustino2020@gmail.com>
2025-02-19 16:50:05 -0300

    feaat: responsividade da noticia principal

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 const btn_abrir = document.getElementById("btn-abrir");
 
 btn_abrir.addEventListener("click", () => {
-  const menu = document.getElementById("menu-mobile");
-  const overlay_menu = document.getElementById("overlay-menu");
 
-  if ((menu.style.display = "none")) {
-    menu.style.display = "block";
-    overlay_menu.style.display = "block";
-    btn_abrir.style.display = "none";
+  let tamanho = document.documentElement.scrollWidth;
+
+  if(tamanho >= 796){
+    document.getElementById("overlay-menu").style.display = "none";
+    document.getElementById("btn-abrir").style.display = "none";
+  }else{
+    const menu = document.getElementById("menu-mobile");
+    const overlay_menu = document.getElementById("overlay-menu");
+
+    if ((menu.style.display = "none")) {
+      menu.style.display = "block";
+      overlay_menu.style.display = "block";
+      btn_abrir.style.display = "none";
+    }
   }
+  
 });
 
 const btn_fechar = document.getElementById("btn-fechar");
 
 btn_fechar.addEventListener("click", () => {
-  const menu = document.getElementById("menu-mobile");
-  const overlay_menu = document.getElementById("overlay-menu");
-  menu.style.display = "none";
-  overlay_menu.style.display = "none";
-  btn_abrir.style.display = "block";
+
+  let tamanho = document.documentElement.scrollWidth;
+
+  if(tamanho >= 796){
+    document.getElementById("overlay-menu").style.display = "none";
+    document.getElementById("btn-abrir").style.display = "none";
+  }else{
+    const menu = document.getElementById("menu-mobile");
+    const overlay_menu = document.getElementById("overlay-menu");
+    menu.style.display = "none";
+    overlay_menu.style.display = "none";
+    btn_abrir.style.display = "block";
+  }
+
+  
 });
 
 function rta() { // recurso técnico alternativo
