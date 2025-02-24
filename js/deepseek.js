1975427ad4f240a9ebd4091d83af80aa70249a05
AndreyJustino <andreyjustino2020@gmail.com>
2025-02-24 17:08:41 -0300

    feat: add funcionalidade do botão "ler mais"  e calculo de margin responsiva em deepseek

--- js/deepseek.js
+++ js/deepseek.js
@@ -16,3 +16,12 @@ window.addEventListener("resize", () => {
 window.addEventListener("load", () => {
     calcMargin();
 });
+
+const readMoreButton = document.getElementById("learn-more")
+
+readMoreButton.addEventListener("click", () => {
+    const moreText = document.getElementById("texts")
+    
+    moreText.style.display = "block"
+    readMoreButton.style.display = "none"
+})
